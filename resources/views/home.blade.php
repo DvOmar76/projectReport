465f5114f7c01777a5784533f3b60ad7ef617bdd
DVOMAR <zzomarzz6@gmail.com>
2023-01-12 17:52:33 +0300

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+{{--<main class="sm:container sm:mx-auto sm:mt-10">--}}
+{{--    <div class="w-full sm:px-6">--}}
+
+{{--        @if (session('status'))--}}
+{{--            <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mb-4" role="alert">--}}
+{{--                {{ session('status') }}--}}
+{{--            </div>--}}
+{{--        @endif--}}
+
+{{--    </div>--}}
+{{--</main>--}}
+
+
+<a href="#" class="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt="">
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    </div>
+</a>
+
+
+@endsection
